e1c6f7c0d7f5605e1c12637d5a75241c940100f3
douiebkhadija <66106341+douiebkhadija@users.noreply.github.com>
2020-05-29 14:13:14 +0100

    Add files via upload

---
+++ Nouvelle-Aquitaine.php
@@ -0,0 +1,94 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+     <head>
+
+         <title>Musées</title>
+         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+
+         <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/Accueil.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/musee.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/decouvrez-nous.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/deconnection.php" />
+
+     </head>
+	  
+         <body>
+
+             <div id ="banner1">
+                 <p>MUSEE DE FRANCE</p>
+             </div>
+			 
+     <br><br><br><br><br><br><br><br><br><br><br><br>
+
+             <div>
+                 <ul>
+				     <center>
+                         <li><a href="Accueil.php">Accueil</a></li>
+                         <li><a href="musee.php">Nos Musées</a></li>
+                         <li><a href="decouvrez-nous.php">Découvrez-Nous </a></li>
+                         <li><a href="../musees/login/login.php">Se déconnecter </a></li>
+                     </center>
+                 </ul>
+             </div><br>
+			 
+	 <br><br><br><br><br><br><br>
+
+   
+                 <h1>
+				     <center>Découvrez nos musées selon la région où vous allez !</center>
+				</h1>
+
+<br>
+                 <h2><center> les Musées en Nouvelle Aquitaine" </center></h2>
+ 
+<hr><br><br>
+             <div id="text">
+                  <h3><center>  Les plus beaux  musées de la Nouvelle Aquitaine à découvrir</center></h3>
+			 </div>
+			 
+<br><br><hr><br><br><br>
+
+                     <table>
+					  <center>
+                         <tr>
+                             <td><img SRC="image/n11.png" width="200"  height ="350"></td>
+                             <td><img SRC="image/n22.png" width="200"  height ="350"></td>
+						</tr> 
+		          </center>
+				  </table>
+<br>
+                     <table>
+					 <center>
+                         <tr>
+                             <td><img SRC="image/n33.png" width="200"  height ="350"></td>
+                             <td><img SRC="image/n44.png" width="200"  height ="350"></td>
+                         </tr>
+						 </center>
+                     </table>
+                    
+</div>
+<br><hr><br><br><br>
+
+				     <h3><center>Les musées sont souvent spécialisés, il en existe principalement neuf grandes catégories :</center></h3><br><br>
+                     <p><center>les musées d'archéologie || les musées d'art || les Musée des Beaux-Arts || les musées des arts décoratifs || les musées d'histoire || les musées de sciences || musées d'histoire naturelle || les musées des techniques |
+                         les musées d'ethnologie. </center></p>
+			
+<br><br><br>
+
+             <footer>
+                 <center>
+                     <p><b>MUSEE DE FRANCE</b></p>
+                     <a href="decouvrez-nous.php">Contactez-nous</a>
+                     <p><a href="Accueil.php" >Retour </a></p>
+                 </center>
+              </footer>
+
+         </body>
+		 
+</html>
