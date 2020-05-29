fb2f6d9652b1ec90271f20c3fdd2c47981b465b2
douiebkhadija <66106341+douiebkhadija@users.noreply.github.com>
2020-05-29 14:26:12 +0100

    Add files via upload

---
+++ Hauts-de-France.php
@@ -0,0 +1,96 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+     <head>
+
+         <title>Musées</title>
+         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+         <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/Accueil.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/musee.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/decouvrez-nous.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/deconnection.php" />
+
+     </head>
+	 
+
+	 
+         <body>
+
+             <div id ="banner1">
+                 <p>MUSEE DE FRANCE</p>
+             </div>
+			 
+     <br><br><br><br><br><br><br><br><br><br><br><br>
+
+             <div>
+                 <ul>
+				     <center>
+                         <li><a href="Accueil.php">Accueil</a></li>
+                         <li><a href="musee.php">Nos Musées</a></li>
+                         <li><a href="decouvrez-nous.php">Découvrez-Nous </a></li>
+                         <li><a href="../musees/login/login.php">Se déconnecter </a></li>
+                     </center>
+                 </ul>
+             </div><br>
+			 
+			 
+	 <br><br><br><br><br><br><br>
+
+                 <h1><center>Découvrez nos musées selon la région où vous allez !</center>
+				</h1>
+			<br>
+			  
+                 <h2><center> les Musées en Haut de France </center></h2>
+     <hr><br><br>
+ 
+             <div id="text">
+                 <h3><center>  Les plus beaux  musées du Haut de la France à découvrir</center></h3>
+			 </div>
+			 
+     <br><br><hr><br><br>
+
+            
+                 <center>
+                     <table>
+                         <tr>
+                             <td><img SRC="image/h1.png" width="200"  height ="350"></td>
+                             <td><img SRC="image/h2.png" width="200"  height ="350"></td>
+                         </tr>
+					 </table>
+                 </center>			  
+<br>
+                 <center>
+                      <table>
+                         <tr>
+                             <td><a><img SRC="image/h3.png" width="200"  height ="350"></td>
+                             <td><a><img SRC="image/h4.png" width="200"  height ="350"></td>
+                         </tr>
+                      </table>
+                 </center>
+        
+
+<br><hr><br><br><br>
+
+				     <h3><center>Les musées sont souvent spécialisés, il en existe principalement neuf grandes catégories :</center></h3><br><br>
+                     <p><center>les musées d'archéologie || les musées d'art || les Musée des Beaux-Arts || les musées des arts décoratifs || les musées d'histoire || les musées de sciences || musées d'histoire naturelle || les musées des techniques |
+                         les musées d'ethnologie. </center></p>
+				
+			 
+<br><br><br>
+
+             <footer>
+                 <center>
+                     <p><b>MUSEE DE FRANCE</b></p>
+                     <a href="decouvrez-nous.php">Contactez-nous</a>
+                     <p><a href="Accueil.php" >Retour </a></p>
+                 </center>
+              </footer>
+
+         </body>
+		 
+</html>
