b66f0328ae6f26370b597b3930985fc33c71798a
douiebkhadija <66106341+douiebkhadija@users.noreply.github.com>
2020-05-29 14:29:10 +0100

    Add files via upload

---
+++ bretagne.php
@@ -0,0 +1,99 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+
+     <head>
+
+         <title>Musées</title>
+         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+
+         <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/Accueil.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/musee.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/decouvrez-nous.php" />
+         <link rel="file:///C:/MAMP/htdocs/Musees/deconnection.php" />
+
+     </head>
+	 
+
+	 
+         <body>
+
+             <div id ="banner1">
+                 <p>MUSEE DE FRANCE</p>
+             </div>
+			 
+     <br><br><br><br><br><br><br><br><br><br><br><br>
+
+             <div>
+                 <ul>
+				     <center>
+                         <li><a href="Accueil.php">Accueil</a></li>
+                         <li><a href="musee.php">Nos Musées</a></li>
+                         <li><a href="decouvrez-nous.php">Découvrez-Nous </a></li>
+                         <li><a href="../musees/login/login.php">Se déconnecter </a></li>
+                     </center>
+                 </ul>
+             </div><br>
+			 
+			 
+	 <br><br><br><br><br><br><br>
+
+                 <h1><center>Découvrez nos musées selon la région où vous allez !</center></h1>
+<br>
+
+     			  <h2><center> les Musées en Bretagne  </center></h2></div>
+ 
+<hr>
+ 
+      
+             <div id="text">
+                 <h3><center>  La Bretagne est une entité géographique et culturelle,<br> et une des nations dites celtiques. 
+                                 Elle occupe une péninsule, à l'extrémité ouest de la France, <br>située entre la Manche au nord, la mer Celtique
+                                 et la mer d'Iroise à l'ouest et le golfe de Gascogne au sud.
+
+                 </center></h3>
+			 </div>
+			 
+
+<hr><br>
+
+                 <center>
+                     <table>
+                         <tr>
+                             <td><img SRC="image/bretagne1.png" width="200"  height ="350"></td>
+                             <td><img SRC="image/bretagne2.png" width="200"  height ="350"></td>
+                             <td><img SRC="image/bretagne3.png" width="200"  height ="350"></td>
+                         </tr>
+                     </table>
+                 </center>
+    
+
+
+
+  
+<hr><br>
+             
+		
+                     <h3><center>Les musées sont souvent spécialisés, il en existe principalement neuf grandes catégories :</center></h3><br><br>
+                     <p><center>les musées d'archéologie || les musées d'art || les Musée des Beaux-Arts || les musées des arts décoratifs || les musées d'histoire || les musées de sciences || musées d'histoire naturelle || les musées des techniques |
+                         les musées d'ethnologie. </center></p>
+			  
+
+     <br><br><br>
+
+             <footer>
+                 <center>
+                     <p><b>MUSEE DE FRANCE</b></p>
+                     <a href="decouvrez-nous.php">Contactez-nous</a>
+                     <p><a href="Accueil.php" >Retour </a></p>
+                 </center>
+             </footer>
+
+ 
+         </body>
+		 
+</html>
